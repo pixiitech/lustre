34627d4f0c98e7d1d17c8fd50d45836bb80b206a
Gregory Hedrick <gregory@Gregorys-MBP.hsd1.fl.comcast.net>
2024-06-12 10:45:07 -0400

    Remove blogs demo

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -3,8 +3,6 @@ import vuetify from "~/plugins/vuetify";
 import './style.css';
 
 import App from '~/app'
-const BlogsIndex = defineAsyncComponent(() => import("~/pages/blogs/index"));
-const BlogsShow = defineAsyncComponent(() => import("~/pages/blogs/show"));
 const PagesIndex = defineAsyncComponent(() => import("~/pages/index"));
 
 const app = createApp({});
@@ -13,7 +11,5 @@ app.use(vuetify);
 
 app.component("App", App);
 app.component("PagesIndex", PagesIndex);
-app.component("BlogsIndex", BlogsIndex);
-app.component("BlogsShow", BlogsShow);
 
 app.mount("#app");
