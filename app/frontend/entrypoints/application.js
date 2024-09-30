122f3146344512a5cefd5f774b0c55d78a763299
Gregory Hedrick <gregory@Gregorys-MBP.hsd1.fl.comcast.net>
2024-09-30 16:18:19 -0400

    Remove 'App' from application.js

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -2,7 +2,7 @@ import { createApp, defineAsyncComponent } from "vue";
 import vuetify from "../plugins/vuetify";
 import './style.css';
 
-import App from '../app'
+// import App from '../app'
 const PagesIndex = defineAsyncComponent(() => import("../pages/index"));
 const CoinDetails = defineAsyncComponent(() => import("../components/coinDetails"));
 
@@ -10,7 +10,7 @@ const app = createApp({});
 
 app.use(vuetify);
 
-app.component("App", App);
+// app.component("App", App);
 app.component("PagesIndex", PagesIndex);
 app.component("CoinDetails", CoinDetails);
 
